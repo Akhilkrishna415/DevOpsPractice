75af93ce90fb3664a16f3b275df28e9022580bed
Akhilkrishna415 <51918938+Akhilkrishna415@users.noreply.github.com>
2023-01-24 23:54:48 -0500

    Update check_pr_files.py

--- check_pr_files.py
+++ check_pr_files.py
@@ -2,45 +2,53 @@ import requests
 import json
 import re
 
-# Github token
-token = "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"
+def get_files_in_branch(owner, repo, branch, token):
+    headers = {"Authorization": f"Token {token}"}
+    url = f"https://api.github.com/repos/{owner}/{repo}/contents/file_pattern_test?ref={branch}"
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        print(f"An error occurred while fetching the files in the {branch} branch.")
+        exit(1)
+    files = json.loads(response.text)
+    all_files = []
+    for file in files:
+        if file["type"] == "dir":
+            # Recursively get files in subfolder
+            all_files += get_files_in_subfolder(owner, repo, branch, file["path"], token)
+        else:
+            all_files.append(file)
+    return all_files
 
-# Repository owner and name
-owner = "Akhilkrishna415"
-repo = "DevOpsPractice"
+def get_files_in_subfolder(owner, repo, branch, path, token):
+    headers = {"Authorization": f"Token {token}"}
+    url = f"https://api.github.com/repos/{owner}/{repo}/contents/{path}?ref={branch}"
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        print(f"An error occurred while fetching the files in the {path} subfolder.")
+        exit(1)
+    files = json.loads(response.text)
+    all_files = []
+    for file in files:
+        if file["type"] == "dir":
+            # Recursively get files in subfolder
+            all_files += get_files_in_subfolder(owner, repo, branch, file["path"], token)
+        else:
+            all_files.append(file)
+    return all_files
 
-pr_number = 2
-# Main branch name
-# main_branch = "main"
+def compare_branch_files(owner, repo, pr_number, token):
+    # Get the files in Pull request
+    url = f"https://api.github.com/repos/{owner}/{repo}/pulls/{pr_number}/files"
+    headers = {"Authorization": f"Token {token}"}
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        print("An error occurred while fetching the pull request data.")
+        exit(1)
+    pull_request_files = json.loads(response.text)
 
-headers = {"Authorization": f"Token {token}"}
+    main_branch_files = get_files_in_branch(owner, repo, "main", token)
 
-# Get the files in Pull request
-url = f"https://api.github.com/repos/{owner}/{repo}/pulls/{pr_number}/files"
-
-response = requests.get(url, headers=headers)
-
-if response.status_code != 200:
-    print("An error occurred while fetching the pull request data.")
-    exit(1)
-
-pull_request_files = json.loads(response.text)
-
-
-# Get the files in main branch
-url = f"https://api.github.com/repos/{owner}/{repo}/contents/file_pattern_test?ref=main"
-response = requests.get(url, headers=headers)
-
-
-if response.status_code != 200:
-    print("An error occurred while fetching the files in the main branch.")
-    exit(1)
-
-main_branch_files = json.loads(response.text)
-
-for file in pull_request_files:
-    # Only consider files in the migrations folder
-    if "file_pattern_test" in file["filename"]:
+    for file in pull_request_files:
         # Extract the unique number prefix from the file name
         pull_request_file_version = re.search(r'[vmu](\d+)__', file["filename"]).group(1)
         match_found = False
@@ -54,5 +62,12 @@ for file in pull_request_files:
         if match_found:
             print(f"WARNING: A file with the same version number prefix '{pull_request_file_version}' already exists in the main branch: {file['filename']}")
 
-        # if match_found is False:
-        #     print("something is wrong")
+# Github token
+token = "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"
+
+# Repository owner and name
+owner = "Akhilkrishna415"
+repo = "DevOpsPractice"
+
+pr_number = 4
+compare_branch_files(owner, repo, pr_number, token)
